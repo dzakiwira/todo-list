ad012744b9f007cba28c3edd234550a8cb43ae2c
Dzaki Wiradinata <dzaki.wiradinata@gmail.com>
2022-10-03 16:16:11 -0400

    Can now add get tasks from user and display in better format

--- src/render.js
+++ src/render.js
@@ -4,8 +4,10 @@ import Todo from "./todo";
 
 const projectList = new Todo();
 
-projectList.addProject(new Project('Project 1', 'Test test test test'));
-projectList.addProject(new Project('Project 2', 'Test test test test'));
+let currentProject = '';
+
+projectList.addProject(new Project('Shopping List', 'For halloween party'));
+projectList.addProject(new Project('LeetCode', 'Interview coming up must be competent'));
 
 loadProjects(projectList);
 
@@ -18,16 +20,14 @@ export function loadProjects(projects) {
         newProject.classList.add('project-nav');
         newProject.dataset.value = index;
         newProject.addEventListener('click', () => {
-            loadMain(project)
+            currentProject = project.getName();
+            loadMain(project);
         });
         newProject.textContent = project.getName();
-        project.addTask(new Task('Example Task', 'Jan 1 2023'));
-        project.addTask(new Task('Example Task 2', 'Jan 1 2023'));
         projectSidebarDiv.appendChild(newProject);
     });
-
     console.log(projectList);
-}
+};
 
 function loadMain(project) {
     const mainTitle = document.querySelector('.main-title');
@@ -35,8 +35,9 @@ function loadMain(project) {
     
     mainTitle.textContent = project.getName();
     mainDesc.textContent = project.getDesc();
+    mainDesc.style.borderBottom = 'solid';
     loadTasks(project);
-}
+};
 
 function loadTasks(project) {
     const mainContent = document.querySelector('.main-content');
@@ -44,10 +45,38 @@ function loadTasks(project) {
 
     project.getTaskList().forEach((task, index) => {
         const newTask = document.createElement('div');
-        
         newTask.classList.add('task');
         newTask.dataset.value = index
-        newTask.textContent = task.getTaskDesc();
+
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.name = 'task-status'
+        checkbox.id = 'task-status';
+
+        const taskDesc = document.createElement('div');
+        taskDesc.textContent = task.getTaskDesc();
+        taskDesc.contentEditable = 'true';
+
+        const taskDate = document.createElement('div');
+        taskDate.classList.add('task-date');
+        taskDate.textContent = task.getDate();
+
+        const deleteTask = document.createElement('div');
+        deleteTask.classList.add('delete-task');
+        deleteTask.innerHTML += '<span class="material-symbols-outlined">delete</span>'
+
+        const leftTask = document.createElement('div');
+        leftTask.classList.add('left-task');
+        const rightTask = document.createElement('div');
+        rightTask.classList.add('right-task');
+
+
+        leftTask.appendChild(checkbox);
+        leftTask.appendChild(taskDesc);
+        rightTask.appendChild(taskDate);
+        rightTask.appendChild(deleteTask);
+        newTask.appendChild(leftTask);
+        newTask.appendChild(rightTask);
         mainContent.appendChild(newTask);
     });
 };
@@ -58,8 +87,20 @@ export function getUserProject() {
     
     addNewProject(projectName, projectDesc);
     loadProjects(projectList);
-}
+};
 
 function addNewProject(projectName, projectDesc) {
     projectList.addProject(new Project(projectName, projectDesc));
-}
+};
+
+export function getUserTask() {
+    const taskDesc = document.getElementById('todo').value;
+    const taskDate = document.getElementById('due-date').value;
+    
+    addNewTask(taskDesc, taskDate);
+    loadTasks(projectList.getProject(currentProject));
+};
+
+function addNewTask(taskDesc, date) {
+    projectList.getProject(currentProject).addTask(new Task(taskDesc, date));
+};
